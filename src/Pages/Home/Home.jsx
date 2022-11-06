0ea1c59b27105a5a055cc14b6b74c64a923b0aad
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-11-06 17:49:06 +0100

    token generated from the client side after login and saved to ls

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,50 @@
-import React from "react";
+import { GoogleAuthProvider } from "firebase/auth";
+import React, { useContext } from "react";
+import { AuthContext } from "../../Contexts/AuthProvider";
 
 const Home = () => {
+  const { socialProvider } = useContext(AuthContext);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const handleGoogle = () => {
+    const googleLogin = async () => {
+      try {
+        const result = await socialProvider(googleProvider);
+        const user = result?.user;
+        const payLoadData = {
+          email: user?.email,
+        };
+
+        // ** Ask for jwt token
+
+        const sendPayload = async () => {
+          try {
+            const response = await fetch(`http://localhost:15000/jwt`, {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(payLoadData),
+            });
+
+            const data = await response.json();
+            const token = data.token.token;
+
+            // ** save the token into ls
+            localStorage.setItem("jwt-token", token);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        sendPayload();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    googleLogin();
+  };
+
   return (
     <section className="dark:bg-slate-900 dark:text-gray-100">
       <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
@@ -13,8 +57,11 @@ const Home = () => {
           To make events please login and register
         </p>
         <div className="flex flex-wrap justify-center">
-          <button className="px-8 py-3 m-2 text-lg font-semibold rounded bg-violet-300 dark:bg-violet-400 dark:text-gray-900">
-            Sign In
+          <button
+            onClick={handleGoogle}
+            className="px-8 py-3 m-2 text-lg font-semibold rounded bg-violet-300 dark:bg-violet-400 dark:text-gray-900"
+          >
+            Google
           </button>
           <button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-gray-700">
             Sign Up
